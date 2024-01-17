c8e535fd324b49086077083aef78cd395ed62889
Digital <127893511+ElbrusO@users.noreply.github.com>
2024-01-17 13:46:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const buttonCheck = document.querySelector('.check');
+const buttonRestart = document.querySelector('.again');
+const colorBack = document.querySelector('.body');
+
+let randomNum = Math.floor(Math.random() * 21);
+let winScore = 0;
+let doGameLol = true;
+
+buttonCheck.addEventListener('click', function() {
+    if (doGameLol === true) {
+
+        const hotORcool = document.querySelector('.message');
+        const inputGuess = document.querySelector('.guess');
+        const inputNum = inputGuess.value;
+
+        inputGuess.value = ''; 
+        inputGuess.focus();
+
+        const centrNumber = document.querySelector('.number');
+        const scoreHigh = document.querySelector('.highscore');
+        const scoreNum = document.querySelector('.score');
+        let score = scoreNum.textContent;
+
+        if (inputNum < randomNum) {
+            hotORcool.textContent = 'Маленькое число';
+
+            if (score <= 0) {
+                doGameLol = false;
+            } else {
+                scoreNum.textContent = score - 1;
+            }
+            
+        } else if (inputNum > randomNum) {
+            hotORcool.textContent = 'Большое число';
+
+            if (score <= 0) {
+                doGameLol = false;
+            } else {
+                scoreNum.textContent = score - 1;
+            }
+            
+        } else {
+            hotORcool.textContent = 'Вы угадали число!';
+
+            doGameLol = false;
+
+            colorBack.classList.add('green');
+            centrNumber.textContent = randomNum;
+
+            if (winScore <= score) {
+                winScore = score;
+                scoreHigh.textContent = winScore;
+            }
+        }
+    }
+    // Слышь - а если попытки закончаться, то что тогда ? - Готово (line 29-30, 38-39) Вот тебе и булевое применение;
+    else { alert('Нажмите -> Снова! <- для продолжения'); }
+});
+
+buttonRestart.addEventListener('click', function () {
+
+    const scoreNum = document.querySelector('.score');
+    const centrNumber = document.querySelector('.number');
+
+    randomNum = Math.floor(Math.random() * 21);
+    doGameLol = true;
+
+    colorBack.classList.remove('green');
+    scoreNum.textContent = 20;
+    centrNumber.textContent = '?';
+});
